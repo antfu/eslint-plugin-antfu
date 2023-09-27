302edec50c278e06cafbd19352338b1ea7601ef9
Jacob Bowdoin <7559478+jacob-8@users.noreply.github.com>
2023-09-27 20:29:34 +0800

    update createEslintRule to emit proper urls

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,9 @@
 import { ESLintUtils } from '@typescript-eslint/utils'
 
+const hasDocs = ['consistent-list-newline']
+
 export const createEslintRule = ESLintUtils.RuleCreator(
-  ruleName => ruleName,
+  ruleName => hasDocs.includes(ruleName)
+    ? `https://github.com/antfu/eslint-plugin-antfu/blob/main/src/rules/${ruleName}.md`
+    : `https://github.com/antfu/eslint-plugin-antfu/blob/main/src/rules/${ruleName}.test.ts`,
 )
