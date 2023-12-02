0d053c5a099f967f24bcafb19715f3d0f1705afd
Anthony Fu <anthonyfu117@hotmail.com>
2023-12-02 03:52:18 +0100

    chore: update

--- eslint.config.js
+++ eslint.config.js
@@ -4,4 +4,12 @@ export default antfu(
   {
     ignores: ['vendor'],
   },
+  {
+    // TODO: remove when migrated to ESLint Stylistic
+    rules: {
+      'antfu/indent-binary-ops': 'off',
+      'antfu/generic-spacing': 'off',
+      'antfu/named-tuple-spacing': 'off',
+    },
+  },
 )
---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import type { ESLint, Linter } from 'eslint'
+import { version } from '../package.json'
+import ifNewline from './rules/if-newline'
+import importDedupe from './rules/import-dedupe'
+import topLevelFunction from './rules/top-level-function'
+import noImportNodeModulesByPath from './rules/no-import-node-modules-by-path'
+import noTsExportEqual from './rules/no-ts-export-equal'
+import consistentListNewline from './rules/consistent-list-newline'
+
+const plugin = {
+  meta: {
+    name: 'antfu',
+    version,
+  },
+  rules: {
+    'consistent-list-newline': consistentListNewline,
+    'if-newline': ifNewline,
+    'import-dedupe': importDedupe,
+    'no-import-node-modules-by-path': noImportNodeModulesByPath,
+    'no-ts-export-equal': noTsExportEqual,
+    'top-level-function': topLevelFunction,
+  },
+} satisfies ESLint.Plugin
+
+export default plugin
+
+type RuleDefinitions = typeof plugin['rules']
+
+export type RuleOptions = {
+  [K in keyof RuleDefinitions]: RuleDefinitions[K]['defaultOptions']
+}
+
+export type Rules = {
+  [K in keyof RuleOptions]: Linter.RuleEntry<RuleOptions[K]>
+}
