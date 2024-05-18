88971ce086a65a97655f7ced5e191fd5cd01ec69
Stephen Zhou <hi@hyoban.cc>
2024-05-18 23:13:26 +0800

    fix: handle TSTypeLiteral

--- src/rules/consistent-list-newline.test.ts
+++ src/rules/consistent-list-newline.test.ts
@@ -142,10 +142,17 @@ const invalid: InvalidTestCase[] = [
   'const foo = (\na, b): {a:b} => {}',
   'interface Foo {\na: 1,b: 2\n}',
   {
-    description: 'Add delimiter to avoid syntax error',
+    description: 'Add delimiter to avoid syntax error, (interface)',
     code: 'interface Foo {a: 1\nb: 2\n}',
     output: o => expect(o)
       .toMatchInlineSnapshot(`"interface Foo {a: 1,b: 2,}"`),
+  },
+  {
+    description: 'Add delimiter to avoid syntax error, (type)',
+    code: 'type Foo = {a: 1\nb: 2\n}',
+    output: o => expect(o)
+      .toMatchInlineSnapshot(`"type Foo = {a: 1,b: 2,}"`),
+    only: true,
   },
   {
     description: 'Delimiter already exists',
--- src/rules/consistent-list-newline.ts
+++ src/rules/consistent-list-newline.ts
@@ -71,7 +71,7 @@ export default createEslintRule<Options, MessageIds>({
 
     function getDelimiter(root: TSESTree.Node, current: TSESTree.Node) {
       let delimiter: string | undefined
-      if (root.type === 'TSInterfaceDeclaration') {
+      if (root.type === 'TSInterfaceDeclaration' || root.type === 'TSTypeLiteral') {
         const currentContent = context.sourceCode.text.slice(current.range[0], current.range[1])
         if (!currentContent.match(/,|;$/))
           delimiter = ','
