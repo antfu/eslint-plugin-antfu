fbb0fca7aede336f046efea79a59ca09792ae5a0
Stephen Zhou <hi@hyoban.cc>
2024-05-18 23:05:38 +0800

    test

--- src/rules/consistent-list-newline.test.ts
+++ src/rules/consistent-list-newline.test.ts
@@ -146,14 +146,12 @@ const invalid: InvalidTestCase[] = [
     code: 'interface Foo {a: 1\nb: 2\n}',
     output: o => expect(o)
       .toMatchInlineSnapshot(`"interface Foo {a: 1,b: 2,}"`),
-    only: true,
   },
   {
     description: 'Delimiter already exists',
     code: 'interface Foo {a: 1;\nb: 2,\nc: 3}',
     output: o => expect(o)
       .toMatchInlineSnapshot(`"interface Foo {a: 1;b: 2,c: 3}"`),
-    only: true,
   },
   'type Foo = {\na: 1,b: 2\n}',
   'type Foo = [1,2,\n3]',
